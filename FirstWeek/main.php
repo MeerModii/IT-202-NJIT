89dfb8ccad5546a4ce11d25eb3eb3e891ba6217a
Meer Modi <116285279+MeerModii@users.noreply.github.com>
2023-09-08 16:32:22 -0400

    classwork

--- FirstWeek/main.php
+++ FirstWeek/main.php
@@ -1,3 +1,15 @@
 <?php
-    echo '<p> Welcome to PHP</p>'; 
+    // get name from the request
+    $first_name = $_GET['first_name'];
+    $last_name = $_GET['last_name'];
 ?>
+<html>
+    <head>
+        <title>Name test</title>
+    </head>
+    <body>
+        <h2>First Name <?php echo $first_name; ?></h2>
+        <h2>Last Name <?php echo $last_name; ?></h2>
+
+    </body>
+</html>
