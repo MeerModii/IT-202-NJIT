ef7a3600da0c92790fc76bbe4ee111bc9fb13a14
Meer Modi <116285279+MeerModii@users.noreply.github.com>
2023-10-11 09:42:03 -0400

    weekfour

---
+++ FourthWeek/add_product_form.php
@@ -0,0 +1,37 @@
+<?php 
+    require('batabase.php');
+    $query = 'SELECT * FROM categories ORDER BY categoryID';
+    $statement = $db->prepare($query);
+    $statement->execute();
+    $categories = $statement->fetchAll();
+    $statement->closeCursor();
+?>
+<html>
+    <head>
+        <title>My Guitar Shop</title>
+    </head>
+    <body>
+        <h1>Product Manager</h1>
+        <form action = "addProduct.php" method = "post">
+            <label>Category:</label>
+            <select name = "category_id">
+                <?php foreach($categories as $category) : ?>
+                <option value = "<?php echo $category['categoryID']; ?>">
+                    <?php echo $category['categoryName']; ?>
+                </option>
+                <?php endforeach; ?>
+            </select>
+            <br>
+            <label>Product Code</label>
+            <input type="text" name="code">
+            <br>
+            <label>Product Name</label>
+            <input type="text" name="name">
+            <br>
+            <label>List Price</label>
+            <input type="text" name="price">
+            <br>
+            <input type="submit"value = "Add Product">
+        </form>
+    </body>
+</html>
--- FourthWeek/product_list.php
+++ FourthWeek/product_list.php
@@ -24,5 +24,57 @@
     $category = $statement2->fetch();
     $statement2 -> closeCursor();
 
+    // Get all categories
+    $queryAllCategories = 'SELECT * FROM categories ORDER BY categoryID';
+    $statement2 = $db->prepare($queryAllCategories);
+    $statement2->execute();
+    $categories = $statement2->fetchAll();
+    $statement2->closeCursor();
+    echo "<pre>"; print_r($categories); echo "</pre>";
 
+    $queryProduct = 'SELECT * FROM products WHERE categoryID = :category_id ORDER BY productID';
+    $statement3 = $db->prepare($queryProduct);
+    $statement3->bindValue(':category_id',$category_id);
+    $statement3->execute();
+    $products = $statement3->fetchAll();
+    $statement3->closeCursor();
 ?>
+<html>
+    <head>
+        <title>My Guitar Shop</title>
+    </head>
+    <body>
+        <main>
+            <h1>Product List</h1>
+            <h2>Categories</h2>
+            <ul>
+                <?php foreach ($categories as $category) : ?>
+                    <li>
+                        <a href="?category_id=<?php echo $category['categoryID']; ?>">
+                        <?php echo $category['categoryName']; ?></a>
+                    </li>
+                <?php endforeach; ?>
+            </ul>
+            <section>
+                <!-- HTML TABLE -->
+                <h2><?php echo $category_name; ?></h2>
+                <table>
+                    <tr>
+                        <th>Code</th>
+                        <th>Name</th>
+                        <th>Price</th>
+                    </tr>
+                        <?php foreach ($products as $product) :?>
+                            <tr>
+                                <td><?php echo $product['productCode']; ?></td>
+                                <td><?php echo $product['productName']; ?></td>
+                                <td><?php echo $product['listPrice']; ?></td>
+                            </tr>
+                        <?php endforeach; ?>
+                </table>
+            </section>
+
+            
+        </main>
+    </body>
+</html>
